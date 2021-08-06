1d1d7b8b79381a2c5df047f021c53e24c35f7625
widuu <admin@widuu.com>
2021-08-06 19:35:52 +0800

    每天写一点加配置

--- src/config.php
+++ src/config.php
@@ -1,1 +1,28 @@
 <?php
+
+return [
+    // api 名称
+    'api_name'           => '',
+    // api 接口请求地址
+    'api_url'            => '',
+    // api 作者
+    'api_author'         => '',
+    // 默认语言
+    'api_language'       => 'zh-cn',
+    // 将哪个模块作为api使用
+    'module_name'        => 'index',
+    // api 路由
+    'api_route'          => '/api',
+    // api 路由绑定域名
+    'api_route_domain'   => '',
+    // api 自动生成地址的后缀
+    'api_url_suffix'      => false,
+    // api 排除类中的方法，譬如你有个 init 等等
+    'api_method_fileter'  => [],
+    // api 名称
+    'api_cache_name'     => 'THINK_APIDOC_CACHE',
+    // 附加类库
+    'extend_class'       => [
+
+    ],
+];
